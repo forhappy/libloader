be6cfe594c229536bc3897a93a6a55101acb6753
Wang Nan <pi3orama@gmail.com>
2010-02-08 16:31:07 +0800

    make loader linkable

--- src/interp/loader.c
+++ src/interp/loader.c
@@ -3,7 +3,16 @@
  * by WN @ Feb. 08, 2010
  */
 
+#include <config.h>
+#include <common/defs.h>
+#include <asm/syscall.h>
 
+__attribute__((used, unused)) int
+xmain(uintptr_t oldesp, volatile uintptr_t retcode)
+{
+	INTERNAL_SYSCALL_int80(exit, 1, 0);
+	return 0;
+}
 
 // vim:ts=4:sw=4
 
