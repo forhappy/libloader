35942d252787ececd500d77f49c98ad1598e5275
Wang Nan <pi3orama@gmail.com>
2010-05-31 01:57:21 +0800

    add new files

---
+++ src/arch/x86/interp/syscalls/fstat64.c
@@ -0,0 +1,11 @@
+/* 
+ * fstat64.c
+ * by WN @ May. 31, 2010
+ */
+DEF_HANDLER(fstat64)
+{
+	
+}
+
+// vim:ts=4:sw=4
+
---
+++ src/arch/x86/interp/syscalls/syscall_handler.h
@@ -0,0 +1,15 @@
+/* 
+ * syscall_handler.h
+ * by WN @ May. 31, 2010
+ */
+
+#ifndef SYSCALL_HANDLER_H
+#define SYSCALL_HANDLER_H
+
+#include <interp/logger.h>
+
+#ifdef PRE_LIBRARY
+#endif
+
+#endif
+
